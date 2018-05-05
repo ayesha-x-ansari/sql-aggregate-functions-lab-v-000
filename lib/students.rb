06f4c9319cc2042405845fae2358ca2fa5cf8bb7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-05 19:43:19 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -6,12 +6,11 @@ end
 
 
 def lowest_student_gpa
-  "SELECT MIN(gpa)  as minimum_gpa FROM students;"
+  "SELECT MIN(gpa)  as minimum_gpa FROM students;" 
 end
 
 def average_student_gpa
-  SELECT average(gpa)
-  FROM students
+  "SELECT AVG(gpa) as average_gpa FROM students;" 
 end
 
 def total_tardies_for_all_students
